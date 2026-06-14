022b92b75303d894b8237c318cbd9deb117945a4
Shrutika Gupta <shrutikagupta.217@gmail.com>
2026-06-14 21:04:10 +0530

    Change environment variable from GEMINI to GOOGLE

--- llm.py
+++ llm.py
@@ -9,10 +9,10 @@ from dotenv import load_dotenv
 # ==============================
 load_dotenv()
 
-api_key = os.getenv("GEMINI_API_KEY")
+api_key = os.getenv("GOOGLE_API_KEY")
 
 if not api_key:
-    raise ValueError("GEMINI_API_KEY not found in .env file")
+    raise ValueError("GOOGLE_API_KEY not found")
 
 genai.configure(api_key=api_key)
 
